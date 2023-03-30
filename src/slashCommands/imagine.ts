b55090b00bc5e5032f03baced5556588e6e4c3b1
IhsenBen <benali.ihsen@gmail.com>
2023-03-30 12:04:24 +0200

    fix dall e slash cmd

--- src/slashCommands/imagine.ts
+++ src/slashCommands/imagine.ts
@@ -1,55 +1,71 @@
-import { ChannelType, EmbedBuilder, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import {
+  EmbedBuilder,
+  PermissionFlagsBits,
+  SlashCommandBuilder,
+} from "discord.js";
 import { SlashCommand } from "../types";
 import { config } from "dotenv";
-config()
+config();
 
 const ImagineCommand: SlashCommand = {
-    command: new SlashCommandBuilder()
-        .setName("imagine")
-        .setDescription("Generate images with Dall-E 2 API")
-        .addStringOption(option =>
-            option.setName('prompt')
-            .setDescription('generates an image')
-            .setRequired(true))
-        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+  command: new SlashCommandBuilder()
+    .setName("imagine")
+    .setDescription("Generate images with Dall-E 2 API")
+    .addStringOption((option) =>
+      option
+        .setName("prompt")
+        .setDescription("generates an image")
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
 
-    execute: interaction => {
-        const prompt = interaction.options.get("prompt");
-                async function generateImage(prompt: string) {
-                    const API_KEY = process.env.OPENAI_API_KEY;
-                    const API_URL = `https://api.openai.com/v1/images/generations`;
-                    try {
-                        if (!API_KEY) throw new Error('No API key provided');
-                        const response = await fetch(API_URL, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'Authorization': `Bearer ${API_KEY}`
-                        },
-                        body: JSON.stringify({
-                            prompt: prompt,
-                            size: "512x512"
-                        })
-                    });
-                    const json = await response.json();
-                    console.log(json.data[0].url);
-                    return json.data[0].url;
-                    } catch (error) {
-                        console.error(error);
-                    }
-                }
-                interaction.reply({
-                    content: `🤔 Let me think...`,
-                    ephemeral: false,
-                  })
-               return generateImage(`${prompt}`).then(url => {
-                    const embed = new EmbedBuilder()
-                        .setImage(url)
-                        .setDescription(`Here is your generated image for **"${prompt?.value}"**:`)
-                    interaction.editReply({ embeds: [embed], content: '<:requisitos:994730420606341240>' })
-                })
-                    .catch(error => console.error(error));
-            }
+  execute: (interaction) => {
+    const prompt = interaction.options.get("prompt")?.value;
+    if(!prompt) return interaction.reply({content: "Please provide a prompt", ephemeral: true})
+    async function generateImage(prompt: string) {
+      const API_KEY = process.env.OPENAI_API_KEY;
+      const API_URL = `https://api.openai.com/v1/images/generations`;
+      try {
+        if (!API_KEY) throw new Error("No API key provided");
+        const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${API_KEY}`,
+        },
+        body: JSON.stringify({
+            prompt: prompt,
+            size: "512x512",
+        }),
+        });
+        const json = await response.json();
+        console.log(json.data[0].url);
+        return json.data[0].url;
+      } catch (error) {
+        console.error(error);
+      }
     }
+    interaction.reply({
+      content: `🤔 Let me think...`,
+      ephemeral: false,
+    });
+    return generateImage(`${prompt}`)
+      .then((url) => {
+        const embed = new EmbedBuilder()
+          .setImage(url)
+          .setDescription(`Here is your generated image for **"${prompt}"**:`);
+        interaction.editReply({
+          embeds: [embed],
+          content: "<:requisitos:994730420606341240>",
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+        interaction.editReply({
+          content: " :( Oops, something went wrong.",
+        });
+      });
+  },
+};
 
 export default ImagineCommand;
